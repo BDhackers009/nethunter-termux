37fd11a2edba6d09b589c3d87655f7dd8db4a799
BDhackers009 <67186139+BDhackers009@users.noreply.github.com>
2021-10-25 01:56:33 +0600

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,1 @@
-pkg up -y && pkg in git proot -y && cd $HOME && git clone https://github.com/BDhaCkers009/proot-distro.git  && bash ~/proot-distro/install.sh && rm -rf ~/proot-distro && proot-distro install kali 
+pkg update -y && pkg upgrade  -y && pkg in git proot -y && cd $HOME && git clone https://github.com/BDhaCkers009/proot-distro.git  && bash ~/proot-distro/install.sh && rm -rf ~/proot-distro && proot-distro install kali 
